eaea752dd226cdb58d8a20d994ceee388768928b
FerrerMark <ferrerjohnmark26@gmail.com>
2025-06-29 21:16:24 +0800

    m

--- scripts/projects.js
+++ scripts/projects.js
@@ -8,7 +8,7 @@ const projects = [
     {
         title: "DIY Resume using React JS",
         description: "Simple resume with React",
-        image: "./img/projects/resume.png",
+        image: "./img/projects/DIYresume.png",
         link: "https://github.com/FerrerMark/my-resume"
     },
     {
